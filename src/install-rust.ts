169363db7d8d05ae82d045db17bbf0ea83a0e635
Jacob Mischka <jacob@mischka.me>
2019-09-27 11:26:27 -0500

    Don't attempt downgrading krb5 anymore
    It fails with `No package available` error on current lambda env,
    and seems to no longer be necessary, my lambdas work fine with the
    omission.

--- src/install-rust.ts
+++ src/install-rust.ts
@@ -16,15 +16,6 @@ async function downloadRustToolchain(version: string = "stable") {
 async function installOpenSSL() {
 	console.log("installing openssl-devel...");
 	try {
-		// need to downgrade otherwise yum can't resolve the dependencies given
-		// a later version is already installed in the machine.
-		await execa(
-			"yum",
-			["downgrade", "-y", "krb5-libs-1.14.1-27.41.amzn1.x86_64"],
-			{
-				stdio: "inherit"
-			}
-		);
 		await execa("yum", ["install", "-y", "openssl-devel"], {
 			stdio: "inherit"
 		});
