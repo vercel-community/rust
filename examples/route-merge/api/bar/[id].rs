0e4bf5aa56949fbaac1eda54e9e8d8de7025e9af
Florentin / 珞辰 <ecklf@icloud.com>
2023-04-13 14:58:03 +0200

    rename paths

--- examples/route-merge/api/bar/[id].rs
+++ examples/route-merge/api/bar/[id].rs
@@ -6,17 +6,18 @@ use vercel_runtime::{Body, Error, Request, Response, StatusCode};
 pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
     let parsed_url = Url::parse(&req.uri().to_string()).unwrap();
     let hash_query: HashMap<String, String> = parsed_url.query_pairs().into_owned().collect();
-    let query_parameter = hash_query.get("path");
+    let query_parameter = hash_query.get("id");
 
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "application/json")
         .body(Body::Text(match query_parameter {
             Some(query_parameter) => format!(
-                "Route is /dynamic/[path] with query parameter `{}`",
+                "Route is /bar/[id] with query parameter `{}`",
                 query_parameter
             ),
-            None => "Route is /dynamic/[path], but query parameter for `path` seems to be missing"
-                .to_string(),
+            None => {
+                "Route is /bar/[id], but query parameter for `id` seems to be missing".to_string()
+            }
         }))?)
 }
